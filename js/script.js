4b3b455a076fc2ece8626631a5e203d1dbaff295
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-01-19 19:26:04 -0600

    Mobile Menu
    Added "hidden" class on medium screens while in mobile menu.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const logo = document.getElementById('logo');
 
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+// Hamburger button listener
+btn.addEventListener('click', navToggle)
+
 
 function onTabClick(e) {
   tabs.forEach((tab) => {
@@ -20,4 +27,16 @@ function onTabClick(e) {
   e.target.classList.add('border-softRed', 'border-b-4')
   const classString = e.target.getAttribute('data-target')
   document.getElementById('panels').getElementsByClassName(classString)[0].classList.remove('hidden')
+}
+
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg')
+  }
 }
