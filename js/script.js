0cb716e13c3847ce171e5e0c948cc5ac16afdb8c
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-01-19 19:29:33 -0600

    Mobile Menu Logo

--- js/script.js
+++ js/script.js
@@ -35,8 +35,8 @@ function navToggle() {
   menu.classList.toggle('hidden');
 
   if (menu.classList.contains('flex')) {
-    logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+    logo.setAttribute('src', './img/logo-bookmark-footer.svg')
   } else {
-    logo.setAttribute('src', './images/logo-bookmark.svg')
+    logo.setAttribute('src', './img/logo-bookmark.svg')
   }
 }
